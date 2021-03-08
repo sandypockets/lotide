5e27fd50a95b7bdf31ae669a03a6d35f2ea78c42
Zac Alexander <zac@znacreative.com>
2021-03-08 17:58:39 +0000

    Moved testing to test directory

--- middle.js
+++ middle.js
@@ -1,31 +1,5 @@
-const eqArrays = (firstArray, secondArray) => {
-  let accumulator = 0;
-  if (firstArray.length === secondArray.length) {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] === secondArray[i]) {
-        accumulator++;
-      }
-    }
-  }
-  if (accumulator === firstArray.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = (array1, array2) => {
-  let message;
-  let passMsg = `🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`;
-  let failMsg = `🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`;
-  if (eqArrays(array1, array2) === true) {
-    message = passMsg;
-  } else {
-    message = failMsg;
-  }
-  return message;
-};
-// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = (array) => {
   const middleElements = [];
@@ -42,15 +16,7 @@ const middle = (array) => {
     middleElements.push(array[middleIndexEven]);
     return middleElements;
   }
+  return;
 };
 
-/* // Debugging
-// 1 - 2 elements
-console.log(middle([1])); // => []
-console.log(middle([1, 2])); // => []
-// 3+ odd number of elements
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
-// 4+ even number of elements
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4] */
+module.exports = middle;
