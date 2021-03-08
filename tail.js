71bfcfd60a76b048f8d2c5327aa710177b5b47e1
Zac Alexander <zac@znacreative.com>
2021-03-08 14:25:33 +0000

    Moved testing code into test directory

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let tailArr = [];
   for (let i = 1; i < arr.length; i++) {
@@ -14,15 +6,4 @@ const tail = function(arr) {
   return tailArr;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-// Debugging
-console.log(result); // Ensuring array is not modified.
-const words = ["Yo Yo", "This is Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3); // Original array still has 3 elements.
-// Confirming that an empty array returns the expected result.
-const moreDubugging = [];
-tail(moreDubugging);
-assertEqual(moreDubugging.length, 0);
+module.exports = tail;
