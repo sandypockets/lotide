020aecb5905e9ec2fa9b46a0ad98d0964910d007
Zac Alexander <zac@znacreative.com>
2021-03-09 00:08:58 +0000

    Refactorered to use Mocha Chai tests

--- without.js
+++ without.js
@@ -1,32 +1,3 @@
-const eqArrays = (firstArray, secondArray) => {
-  let accumulator = 0;
-  if (firstArray.length === secondArray.length) {
-    for (let i = 0; i < firstArray.length; i++) {
-      if (firstArray[i] === secondArray[i]) {
-        accumulator++;
-      }
-    }
-  }
-  if (accumulator === firstArray.length) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-const assertArraysEqual = (array1, array2) => {
-  let message;
-  let passMsg = `🟢🟢🟢 Assertion Passed: ${array1} === ${array2}`;
-  let failMsg = `🔴🔴🔴 Assertion Failed: ${array1} !== ${array2}`;
-  if (eqArrays(array1, array2) === true) {
-    message = passMsg;
-  } else {
-    message = failMsg;
-  }
-  return message;
-};
-// console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // Debugging
-
 const without = (source, itemsToRemove) => {
   let cleanedArray = [];
   for (let i = 0; i < source.length; i++) {
@@ -41,14 +12,4 @@ const without = (source, itemsToRemove) => {
   return cleanedArray;
 };
 
-
-
-// Test cases
-console.log(without([1, 2, 3], [1])); // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-
-// Make sure the original array was not altered by the without function
-console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
+module.exports = without;
